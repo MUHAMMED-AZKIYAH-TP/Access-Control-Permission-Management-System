a37fd9fa9d0a981ee0c5fdbe30f75c2a003d51eb
MUHAMMED AZKIYAH T.P <azkiyahtp@gmail.com>
2026-01-06 22:10:09 +0530

    fix all errors now

--- Backend/src/codeedex_project/codeedex_project/settings.py
+++ Backend/src/codeedex_project/codeedex_project/settings.py
@@ -151,6 +151,10 @@ CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # -------------------------
 # GENERAL
 # -------------------------
